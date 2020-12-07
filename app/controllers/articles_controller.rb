e276e6e7f199fcd03e850f289d18c1d3c8808c04
CY Kalu <cy23@tutanota.com>
2020-12-07 16:53:28 +0100

    Add actions and views for articles controller
    add the following actions
    - #show
    - #new
    - #create
    - #destroy

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,30 @@
 class ArticlesController < ApplicationController
+  include ArticlesHelper
+
   def index
     @articles = Article.all
   end
 
-  def show 
+  def show
     @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    puts params
+    @article = Article.new(article_params)
+    @article.save
+
+    redirect_to article_path(@article)
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to articles_path
   end
 end
