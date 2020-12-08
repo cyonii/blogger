65bcdf43e36eab7b1fcb1d40c48367d476edfaa6
CY Kalu <cy23@tutanota.com>
2020-12-08 23:29:37 +0100

    Secure app with before_filter
    - secure app using before_action helper to set views and permission for 
    auth users and non-auth users

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,8 +1,10 @@
 class TagsController < ApplicationController
+  before_action :require_login, only: [:destroy]
+
   def index
     @tag = Tag.all
   end
-  
+
   def show
     @tag = Tag.find(params[:id])
   end
